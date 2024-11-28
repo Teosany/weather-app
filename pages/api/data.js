fc33b052deb1bd8469b9b507b3b5e14df40507b1
Teo <teosany@icloud.com>
2024-11-28 03:26:42 +0100

    feat: refactor API call logic and fix data mapping inconsistencies

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,8 +1,85 @@
+const weatherCodeMapping = {
+    0: {icon: "01", description: "Clear sky"},
+    1: {icon: "02", description: "Mainly clear"},
+    2: {icon: "02", description: "Partly cloudy"},
+    3: {icon: "03", description: "Overcast"},
+    45: {icon: "50", description: "Foggy"},
+    48: {icon: "50", description: "Depositing rime fog"},
+    51: {icon: "09", description: "Light drizzle"},
+    53: {icon: "09", description: "Moderate drizzle"},
+    55: {icon: "09", description: "Dense drizzle"},
+    61: {icon: "10", description: "Slight rain"},
+    63: {icon: "10", description: "Moderate rain"},
+    65: {icon: "10", description: "Heavy rain"},
+    71: {icon: "13", description: "Slight snow"},
+    73: {icon: "13", description: "Moderate snow"},
+    75: {icon: "13", description: "Heavy snow"},
+    77: {icon: "13", description: "Snow grains"},
+    80: {icon: "09", description: "Slight rain showers"},
+    81: {icon: "09", description: "Moderate rain showers"},
+    82: {icon: "09", description: "Violent rain showers"},
+    95: {icon: "11", description: "Thunderstorm"},
+    96: {icon: "11", description: "Thunderstorm with slight hail"},
+    99: {icon: "11", description: "Thunderstorm with heavy hail"}
+}
+
+
 export default async function handler(req, res) {
-  const { cityInput } = req.body;
-  const getWeatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
-  );
-  const data = await getWeatherData.json();
-  res.status(200).json(data);
+    const {cityInput} = req.body
+
+    const getWeatherData = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${cityInput.latitude}&longitude=${cityInput.longitude}&\
+current_weather=true&timezone=auto&timeformat=unixtime&daily=sunrise,sunset&minutely_15=visibility,relative_humidity_2m,\
+apparent_temperature`
+    )
+    const data = await getWeatherData.json()
+
+    const mappedIcon = weatherCodeMapping[data.current_weather.weathercode].icon
+    const authenticDescription = weatherCodeMapping[data.current_weather.weathercode].description
+    const mappedIconSuffix = data.current_weather.is_day === 1 ? "d" : "n"
+    const authenticIcon = `${mappedIcon}${mappedIconSuffix}`
+
+    const currentTime = data.current_weather.time
+    const dt = Math.floor(Date.now() / 1000)
+    const timezone = data.utc_offset_seconds
+    const timeIndex = data.minutely_15.time.findIndex(time => time == currentTime)
+
+    const visibility = data.minutely_15.visibility[timeIndex]
+    const humidity = data.minutely_15.relative_humidity_2m[timeIndex]
+    const apparentTemperature = data.minutely_15.apparent_temperature[timeIndex]
+
+    const temp = data.current_weather.temperature
+    const windSpeed = data.current_weather.windspeed
+    const windDeg = data.current_weather.winddirection
+    const sunrise = data.daily.sunrise[0]
+    const sunset = data.daily.sunset[0]
+
+    const responseData = {
+        "weather": [
+            {
+                "description": authenticDescription,
+                "icon": authenticIcon
+            }
+        ],
+        "main": {
+            "temp": temp,
+            "feels_like": apparentTemperature,
+            "humidity": humidity,
+        },
+        "visibility": visibility,
+        "wind": {
+            "speed": windSpeed,
+            "deg": windDeg
+        },
+        "dt": dt,
+        "sys": {
+            "country": cityInput.country,
+            "sunrise": sunrise,
+            "sunset": sunset
+        },
+        "timezone": timezone,
+        "name": cityInput.city,
+    }
+
+    res.status(200).json(responseData)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,79 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
-import { MainCard } from "../components/MainCard";
-import { ContentBox } from "../components/ContentBox";
-import { Header } from "../components/Header";
-import { DateAndTime } from "../components/DateAndTime";
-import { Search } from "../components/Search";
-import { MetricsBox } from "../components/MetricsBox";
-import { UnitSwitch } from "../components/UnitSwitch";
-import { LoadingScreen } from "../components/LoadingScreen";
-import { ErrorScreen } from "../components/ErrorScreen";
+import {MainCard} from "../components/MainCard";
+import {ContentBox} from "../components/ContentBox";
+import {Header} from "../components/Header";
+import {DateAndTime} from "../components/DateAndTime";
+import {MetricsBox} from "../components/MetricsBox";
+import {UnitSwitch} from "../components/UnitSwitch";
+import {LoadingScreen} from "../components/LoadingScreen";
+import {ErrorScreen} from "../components/ErrorScreen";
 
 import styles from "../styles/Home.module.css";
 
 export const App = () => {
-  const [cityInput, setCityInput] = useState("Riga");
-  const [triggerFetch, setTriggerFetch] = useState(true);
-  const [weatherData, setWeatherData] = useState();
-  const [unitSystem, setUnitSystem] = useState("metric");
+    const [cityInput, setCityInput] = useState(null);
+    const [triggerFetch, setTriggerFetch] = useState(true);
+    const [weatherData, setWeatherData] = useState(null);
+    const [unitSystem, setUnitSystem] = useState("metric");
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch("api/data", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ cityInput }),
-      });
-      const data = await res.json();
-      setWeatherData({ ...data });
-      setCityInput("");
-    };
-    getData();
-  }, [triggerFetch]);
+    useEffect(() => {
+        const loadConfig = async () => {
+            const response = await fetch('/config.json');
+            const data = await response.json();
+            setCityInput(data);
+            setTriggerFetch(prev => !prev);
+        };
+        loadConfig();
+    }, []);
 
-  const changeSystem = () =>
-    unitSystem == "metric"
-      ? setUnitSystem("imperial")
-      : setUnitSystem("metric");
+    useEffect(() => {
+        if (!cityInput) return
+        const getData = async () => {
+            const res = await fetch("api/data", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({cityInput}),
+            });
+            const data = await res.json();
+            setWeatherData(data);
+        };
+        getData();
 
-  return weatherData && !weatherData.message ? (
-    <div className={styles.wrapper}>
-      <MainCard
-        city={weatherData.name}
-        country={weatherData.sys.country}
-        description={weatherData.weather[0].description}
-        iconName={weatherData.weather[0].icon}
-        unitSystem={unitSystem}
-        weatherData={weatherData}
-      />
-      <ContentBox>
-        <Header>
-          <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
-          <Search
-            placeHolder="Search a city..."
-            value={cityInput}
-            onFocus={(e) => {
-              e.target.value = "";
-              e.target.placeholder = "";
-            }}
-            onChange={(e) => setCityInput(e.target.value)}
-            onKeyDown={(e) => {
-              e.keyCode === 13 && setTriggerFetch(!triggerFetch);
-              e.target.placeholder = "Search a city...";
-            }}
-          />
-        </Header>
-        <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
-        <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
-      </ContentBox>
-    </div>
-  ) : weatherData && weatherData.message ? (
-    <ErrorScreen errorMessage="City not found, try again!">
-      <Search
-        onFocus={(e) => (e.target.value = "")}
-        onChange={(e) => setCityInput(e.target.value)}
-        onKeyDown={(e) => e.keyCode === 13 && setTriggerFetch(!triggerFetch)}
-      />
-    </ErrorScreen>
-  ) : (
-    <LoadingScreen loadingMessage="Loading data..." />
-  );
+        const interval = setInterval(getData, 3600000);
+        return () => clearInterval(interval);
+
+    }, [triggerFetch, cityInput]);
+
+    const changeSystem = () =>
+        unitSystem == "metric"
+            ? setUnitSystem("imperial")
+            : setUnitSystem("metric");
+
+    return weatherData && !weatherData.message ? (
+        <div className={styles.wrapper}>
+            <MainCard
+                city={weatherData.name}
+                country={weatherData.sys.country}
+                description={weatherData.weather[0].description}
+                iconName={weatherData.weather[0].icon}
+                unitSystem={unitSystem}
+                weatherData={weatherData}
+            />
+            <ContentBox>
+                <Header>
+                    <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
+                </Header>
+                <MetricsBox weatherData={weatherData} unitSystem={unitSystem}/>
+                <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
+            </ContentBox>
+        </div>
+    ) : weatherData && weatherData.message ? (
+        <ErrorScreen errorMessage="City not found, try again!">
+        </ErrorScreen>
+    ) : (
+        <LoadingScreen loadingMessage="Loading data..."/>
+    );
 };
 
 export default App;
